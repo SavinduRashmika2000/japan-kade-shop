0ec923ff3516facf1a8f95ea998b3e9e2aa86617
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-04-18 19:59:51 +0530

    feat(backend): bootstrap BaseAuditEntity entity and basic fields

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/model/BaseAuditEntity.java
@@ -0,0 +1,7 @@
+package com.autocare.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
