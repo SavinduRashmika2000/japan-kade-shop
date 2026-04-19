3f75ece3c5360c49e7db61e5c09855f8e96cda6e
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-04-19 08:18:09 +0530

    feat(backend): add relationships and JPA annotations to Category

--- japan_kade/backend/src/main/java/com/autocare/backend/model/Category.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/model/Category.java
@@ -4,3 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Table(name = "categories")
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
