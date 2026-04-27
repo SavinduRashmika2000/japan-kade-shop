74dc932eed4dc4b2cdd53c51544aff2d199b671a
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-04-27 19:02:10 +0530

    feat(backend): bootstrap JobItemRepository repository interface

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/repository/JobItemRepository.java
@@ -0,0 +1,7 @@
+package com.autocare.backend.repository;
+
+import com.autocare.backend.model.JobCard;
+import com.autocare.backend.model.JobItem;
+import com.autocare.backend.model.StockItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
