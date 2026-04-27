0748c92bd956086baea6e554cf592f6a654059a1
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-04-27 22:10:48 +0530

    feat(backend): declare custom query methods in JobItemRepository

--- japan_kade/backend/src/main/java/com/autocare/backend/repository/JobItemRepository.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/repository/JobItemRepository.java
@@ -5,3 +5,10 @@ import com.autocare.backend.model.JobItem;
 import com.autocare.backend.model.StockItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface JobItemRepository extends JpaRepository<JobItem, Long> {
+    List<JobItem> findByStockItemAndJobCardStatusAndJobCardEndTimeAfter(
