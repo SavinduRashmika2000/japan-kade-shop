74f9ae1004243e03ad0999154a3e1d5ad3f9eabf
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-04-29 22:07:00 +0530

    feat(backend): bootstrap CategoryService service interface and implementation

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.autocare.backend.service;
+
+import com.autocare.backend.model.Category;
+import com.autocare.backend.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
