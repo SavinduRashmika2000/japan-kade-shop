289b865dad33da4232a685f1a8d4fc64ca347a9b
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-04-29 12:56:10 +0530

    feat(backend): bootstrap UserRepository repository interface

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.autocare.backend.repository;
+
+import com.autocare.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
