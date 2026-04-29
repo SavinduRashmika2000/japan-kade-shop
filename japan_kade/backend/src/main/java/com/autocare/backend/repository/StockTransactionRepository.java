634f9809769d72a87cbc9c21e81a62efb44525fd
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-04-29 05:44:51 +0530

    feat(backend): bootstrap StockTransactionRepository repository interface

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/repository/StockTransactionRepository.java
@@ -0,0 +1,5 @@
+package com.autocare.backend.repository;
+
+import com.autocare.backend.model.StockTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
