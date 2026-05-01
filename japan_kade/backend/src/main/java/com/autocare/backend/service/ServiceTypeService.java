c54f87b650f47d9594742feb21338069647558c0
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-01 19:49:06 +0530

    feat(backend): bootstrap ServiceTypeService service interface and implementation

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/service/ServiceTypeService.java
@@ -0,0 +1,8 @@
+package com.autocare.backend.service;
+
+import com.autocare.backend.model.ServiceType;
+import com.autocare.backend.repository.ServiceTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
