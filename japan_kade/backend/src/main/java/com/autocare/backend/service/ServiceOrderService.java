50b988adcbf4a5e2f5a439f8c82e84b59916a217
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-01 14:12:31 +0530

    feat(backend): bootstrap ServiceOrderService service interface and implementation

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/service/ServiceOrderService.java
@@ -0,0 +1,10 @@
+package com.autocare.backend.service;
+
+import com.autocare.backend.model.ServiceOrder;
+import com.autocare.backend.repository.ServiceOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ServiceOrderService {
