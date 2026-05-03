77efa7e32e929cd9c11ce1f267f3dbc5edbee8d9
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-04 00:29:02 +0530

    feat(backend): bootstrap CategoryController controller with basic endpoints

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/controller/CategoryController.java
@@ -0,0 +1,10 @@
+package com.autocare.backend.controller;
+
+import com.autocare.backend.model.Category;
+import com.autocare.backend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
