a79ca227b632dcb7d7c68dfc8d2b87a399399ab7
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-05 00:10:22 +0530

    feat(backend): bootstrap InvoiceController controller with basic endpoints

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/controller/InvoiceController.java
@@ -0,0 +1,13 @@
+package com.autocare.backend.controller;
+
+import com.autocare.backend.model.Invoice;
+import com.autocare.backend.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/invoices")
