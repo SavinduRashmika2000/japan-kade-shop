6ac65ab988cb15960de03e41eeff736bad67ab88
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-05 15:59:41 +0530

    feat(backend): bootstrap JobLogController controller with basic endpoints

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/controller/JobLogController.java
@@ -0,0 +1,7 @@
+package com.autocare.backend.controller;
+
+import com.autocare.backend.model.JobLog;
+import com.autocare.backend.repository.JobLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
