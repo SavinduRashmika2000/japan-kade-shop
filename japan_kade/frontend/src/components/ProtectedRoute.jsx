f9847147b9a9ff204bf2e62d2ecf23deb97fe28f
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-11 16:34:18 +0530

    feat(frontend): create skeleton for ProtectedRoute component

---
+++ japan_kade/frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const ProtectedRoute = ({ children, roles }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) return <div>Loading...</div>;
