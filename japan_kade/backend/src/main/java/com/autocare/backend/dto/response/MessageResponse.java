924ea62261a74a126599fbbea269cf8a65b671cb
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-21 04:51:54 +0530

    chore(backend): add core content to MessageResponse

--- japan_kade/backend/src/main/java/com/autocare/backend/dto/response/MessageResponse.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/dto/response/MessageResponse.java
@@ -4,3 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
