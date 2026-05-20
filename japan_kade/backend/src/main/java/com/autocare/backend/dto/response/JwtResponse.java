a994d58788f275f3eb90caed52825d96147cd90b
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-20 23:44:07 +0530

    chore(backend): refine settings in JwtResponse

--- japan_kade/backend/src/main/java/com/autocare/backend/dto/response/JwtResponse.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/dto/response/JwtResponse.java
@@ -18,3 +18,13 @@ public class JwtResponse {
         this.id = id;
         this.username = username;
         this.email = email;
+        this.name = name;
+        this.roles = roles;
+    }
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
