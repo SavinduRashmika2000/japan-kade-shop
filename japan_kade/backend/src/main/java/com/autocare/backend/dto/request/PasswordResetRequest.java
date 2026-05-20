3fbf68e1b20cb49cb9acd6fa9d8facea31f2ec9a
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-20 06:54:26 +0530

    chore(backend): add core content to PasswordResetRequest

--- japan_kade/backend/src/main/java/com/autocare/backend/dto/request/PasswordResetRequest.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/dto/request/PasswordResetRequest.java
@@ -6,3 +6,11 @@ import lombok.Data;
 @Data
 public class PasswordResetRequest {
     @NotBlank
+    private String phone;
+    
+    @NotBlank
+    private String idNo;
+    
+    private String newPassword;
+
+    public String getPhone() { return phone; }
