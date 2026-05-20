47409283366fbc7ce41c27dec44b2e3703ec19ea
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-20 21:18:49 +0530

    chore(backend): add core content to JwtResponse

--- japan_kade/backend/src/main/java/com/autocare/backend/dto/response/JwtResponse.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/dto/response/JwtResponse.java
@@ -8,3 +8,13 @@ public class JwtResponse {
     private String token;
     private String type = "Bearer";
     private Long id;
+    private String username;
+    private String email;
+    private String name;
+    private List<String> roles;
+
+    public JwtResponse(String accessToken, Long id, String username, String email, String name, List<String> roles) {
+        this.token = accessToken;
+        this.id = id;
+        this.username = username;
+        this.email = email;
