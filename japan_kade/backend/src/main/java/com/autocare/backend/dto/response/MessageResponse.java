5f5cff72875407595d7437a769e523b808aff087
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-21 07:42:23 +0530

    chore(backend): finalize MessageResponse.java

--- japan_kade/backend/src/main/java/com/autocare/backend/dto/response/MessageResponse.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/dto/response/MessageResponse.java
@@ -10,3 +10,7 @@ public class MessageResponse {
     public MessageResponse(String message) {
         this.message = message;
     }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+}
