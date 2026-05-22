8b605011c817e7633db3bd46dedf32b28f1f0bfb
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-22 18:56:48 +0530

    build(backend): define bean definitions and properties in JwtUtils

--- japan_kade/backend/src/main/java/com/autocare/backend/security/JwtUtils.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/security/JwtUtils.java
@@ -30,3 +30,19 @@ public class JwtUtils {
                 .setSubject((userPrincipal.getUsername()))
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build()
+               .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
