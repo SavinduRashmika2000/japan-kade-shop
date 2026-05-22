464c9c26eefd7c8b02f4d8386ced330aded683a3
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-22 08:58:42 +0530

    build(backend): add security rules/properties to AuthTokenFilter

--- japan_kade/backend/src/main/java/com/autocare/backend/security/AuthTokenFilter.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/security/AuthTokenFilter.java
@@ -15,3 +15,22 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+
+                UserDetailsImpl userDetails = (UserDetailsImpl) userDetailsService.loadUserByUsername(username);
