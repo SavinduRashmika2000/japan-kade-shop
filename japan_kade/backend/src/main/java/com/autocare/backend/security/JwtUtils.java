3caebe3eef54199fe692901a5b730cb7916cd815
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-22 15:03:42 +0530

    build(backend): bootstrap JwtUtils configuration

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/security/JwtUtils.java
@@ -0,0 +1,16 @@
+package com.autocare.backend.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
