7afad502c688f54ad040bbff1090de13bcc80f86
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-23 07:57:07 +0530

    build(backend): bootstrap UserDetailsImpl configuration

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/security/UserDetailsImpl.java
@@ -0,0 +1,23 @@
+package com.autocare.backend.security;
+
+import com.autocare.backend.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private boolean enabled;
