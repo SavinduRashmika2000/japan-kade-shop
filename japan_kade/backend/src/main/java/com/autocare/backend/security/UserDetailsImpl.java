e5a11d232b5cb54b95369230f4ab9ca7487aa239
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-23 10:24:09 +0530

    build(backend): add security rules/properties to UserDetailsImpl

--- japan_kade/backend/src/main/java/com/autocare/backend/security/UserDetailsImpl.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/security/UserDetailsImpl.java
@@ -21,3 +21,26 @@ public class UserDetailsImpl implements UserDetails {
     private String password;
 
     private boolean enabled;
+    private String currentToken;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+
+    public UserDetailsImpl(Long id, String username, String email, String password,
+                           boolean enabled, String currentToken,
+                           Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.enabled = enabled;
+        this.currentToken = currentToken;
+        this.authorities = authorities;
+    }
+
+
+    public static UserDetailsImpl build(User user) {
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(user.getRole().name()));
+
+        return new UserDetailsImpl(
+                user.getId(), 
