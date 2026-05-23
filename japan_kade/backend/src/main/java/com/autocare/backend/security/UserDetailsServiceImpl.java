ccd80e7e9e778182129ad0a51e91e4998a3220db
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-23 18:44:59 +0530

    build(backend): bootstrap UserDetailsServiceImpl configuration

---
+++ japan_kade/backend/src/main/java/com/autocare/backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,10 @@
+package com.autocare.backend.security;
+
+import com.autocare.backend.model.User;
+import com.autocare.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
