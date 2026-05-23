f557917e8a40181f24eb71c741a78392af771e02
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-23 21:06:58 +0530

    build(backend): add security rules/properties to UserDetailsServiceImpl

--- japan_kade/backend/src/main/java/com/autocare/backend/security/UserDetailsServiceImpl.java
+++ japan_kade/backend/src/main/java/com/autocare/backend/security/UserDetailsServiceImpl.java
@@ -8,3 +8,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String identifier) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(identifier)
