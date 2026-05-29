0f3f869cd9676d5f1339cbb443fd43623aef53fd
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-29 23:27:02 +0530

    chore(frontend): create customerService foundation

---
+++ japan_kade/frontend/src/services/customerService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import authService from './authService';
+
+const API_URL = 'http://localhost:8080/api/customers';
+
+// Add token to headers
+const authHeader = () => {
+  const user = authService.getCurrentUser();
+  if (user && user.token) {
+    return { Authorization: 'Bearer ' + user.token };
