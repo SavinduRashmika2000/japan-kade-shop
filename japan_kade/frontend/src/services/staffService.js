2ab2f70d463a172733489c4614267dd7d4915156
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-05-31 10:35:06 +0530

    chore(frontend): create staffService foundation

---
+++ japan_kade/frontend/src/services/staffService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/api/auth/';
+
+const getAuthHeader = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  if (user && user.token) {
+    return { Authorization: 'Bearer ' + user.token };
+  } else {
+    return {};
+  }
