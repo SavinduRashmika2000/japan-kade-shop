f59316e5916b10ed1c90b1ceb2fc4a7c973ec0c9
Savindu_Rashmika <savindurashmika2000@gmail.com>
2026-06-01 03:26:23 +0530

    chore(frontend): create supplierService foundation

---
+++ japan_kade/frontend/src/services/supplierService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/api/suppliers';
+
+const getAuthHeader = () => {
+    const userStr = localStorage.getItem('user');
+    if (!userStr) return {};
+    const user = JSON.parse(userStr);
+    return { Authorization: `Bearer ${user.token || user.accessToken}` };
+};
